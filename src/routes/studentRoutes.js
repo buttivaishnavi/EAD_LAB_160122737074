75eba2b004be080eb0dd7ebabd6ce5fe3ee7207b
buttivaishnavi <buttivaishnavi19@gmail.com>
2024-11-07 11:41:20 +0530

    Delete src directory

--- src/routes/studentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createStudent } = require('../controllers/studentController');
-
-// Define the POST route for creating a student
-router.post('/students', createStudent);
-
-module.exports = router;
